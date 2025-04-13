eb0a758e141a58d0fe7714db45ee69ed759318ba
Pushan <mukhopadhyaypushan42@gmail.com>
2025-04-13 16:51:36 +0530

    ui page card design done and next is firebase connection

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React, { ReactNode } from 'react'
 
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className='flex gap-2 items-center'>
+          <Image src={"/logo.svg"} alt="logo" height={32} width={38}/>
+          <h2 className='text-primary-100'>SpeakToCrack</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
   )
 }
 
