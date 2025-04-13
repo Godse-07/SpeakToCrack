6bbdb3ed4f84d84abdccb9b3c235b8af8261fae5
Pushan <mukhopadhyaypushan42@gmail.com>
2025-04-14 00:45:11 +0530

    agent add and testing

---
+++ components/Agent.tsx
@@ -0,0 +1,75 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React from "react";
+
+enum CallStatus {
+  INACTIVE = "INACTIVE",
+  ACTIVE = "ACTIVE",
+  CONNECTING = "CONNECTING",
+  FINISHED = "FINISHED",
+}
+
+interface AgentProps {
+  userName: string;
+}
+
+const Agent = ({ userName }: AgentProps) => {
+  const isSpeaking = true;
+  const callStatus = CallStatus.INACTIVE;
+
+  return (
+    <>
+      <div className="call-view">
+        <div className="card-interviewer">
+          <div className="avatar">
+            <Image
+              src={"/ai-avatar.png"}
+              alt="ai-avatar vapi"
+              width={65}
+              height={54}
+              className="object-cover"
+            />
+            {isSpeaking && <span className="animate-speak"></span>}
+          </div>
+          <h3>Ai Interviewer</h3>
+        </div>
+        <div className="card-border">
+          <div className="card-content">
+            <Image
+              src={"/user-avatar.png"}
+              alt="user-avatar"
+              width={540}
+              height={540}
+              className="rounded-full object-cover size-[120px]"
+            />
+            <h3>{userName}</h3>
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex justify-center ">
+      {callStatus !== "ACTIVE" ? (
+          <button className="relative btn-call">
+            <span
+              className={cn(
+                "absolute animate-ping rounded-full opacity-75",
+                callStatus !== "CONNECTING" && "hidden"
+              )}
+            />
+
+            <span className="relative">
+              {callStatus === "INACTIVE" || callStatus === "FINISHED"
+                ? "Call"
+                : ". . ."}
+            </span>
+          </button>
+        ) : (
+          <button className="btn-disconnect">
+            End
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Agent;
