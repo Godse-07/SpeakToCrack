d239a6149c63f5e221478001b1718df336281282
Pushan <mukhopadhyaypushan42@gmail.com>
2025-04-14 03:09:07 +0530

    voice agent add and firebase store successfully

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,34 +1,43 @@
-import React from "react";
+import { Controller, Control, FieldValues, Path } from "react-hook-form";
+
 import {
-  FormControl,
-  FormDescription,
   FormItem,
   FormLabel,
+  FormControl,
   FormMessage,
-} from "./ui/form";
-import { Input } from "./ui/input";
-import { Control, Controller, FieldValues } from "react-hook-form";
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface FormFieldProps<T extends FieldValues> {
-    control: Control<T>;
-    name: string;
-    label: string;
-    placeholder?: string;
-    type?: 'text' | 'email' | 'password' | 'number' | 'tel' | 'url' | 'file';
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password";
 }
 
-const FormField = ({control, name, label, placeholder, type="text" }: FormFieldProps<T>) => {
+const FormField = <T extends FieldValues>({
+  control,
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: FormFieldProps<T>) => {
   return (
     <Controller
-      name={name}
       control={control}
+      name={name}
       render={({ field }) => (
         <FormItem>
           <FormLabel className="label">{label}</FormLabel>
           <FormControl>
-            <Input placeholder={placeholder} {...field} type={type}/>
+            <Input
+              className="input"
+              type={type}
+              placeholder={placeholder}
+              {...field}
+            />
           </FormControl>
-          {/* <FormDescription>{}</FormDescription> */}
           <FormMessage />
         </FormItem>
       )}
